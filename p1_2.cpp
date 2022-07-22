375fea68fc068abc81ec4deb4a602835ff33510d
Keyur Patel <keyurbhut12345@gmail.com>
2022-07-22 08:29:56 +0000

    Added Files

--- p1_2.cpp
+++ p1_2.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void gcd(int a, int b)
-{
+void gcd(int a, int b){
     int temp, count = 0, int1, int2;
     int1 = a;
     int2 = b;
-    while (b != 0)
-    {
+    while (b != 0){
         temp = b;
         b = a % b;
         a = temp;
@@ -16,8 +14,7 @@ void gcd(int a, int b)
     cout << "GCD of " << int1 << " and " << int2 << " is " << a << " which ran " << count << " times " << endl;
 }
 
-int main()
-{
+int main(){
     int a = 10, b = 15;
     gcd(a, b);
     return 0;
